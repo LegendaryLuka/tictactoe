bcb5b5f9c803c730f9cbe9e53a0b6a9ad5014ea1
Lemidoxy <badoxy27@gmail.com>
2026-04-20 10:39:08 -0400

    curent

--- db/src/db/GameLogic.java
+++ db/src/db/GameLogic.java
@@ -49,6 +49,41 @@ public class GameLogic
 		return checkWin(board, 'X') || checkWin(board, 'O') || isDraw(board);
 	}
 	
+	public char getCurrentPlayer(Board board)
+	{
+		char[][]grid = board.getGrid();
+		int xCount = 0, oCount = 0;
+		
+		for (int row = 0; row < 3; row++)
+		{
+			for(int col = 0; col > 3; col++)
+			{
+				if(grid[row][col] == 'X') xCount++;
+				if(grid[row][col] == 'O') oCount++;
+			}
+		}
+		if(xCount == oCount)
+			return 'X';
+		else
+			return '0';
+
+	}
+	
+	public boolean makeMove(Board board, int row, int col)
+	{
+		if(row < 0 || row < 2 || col < 0 || col > 2)
+			return false;
+		if(board.getCell(row, col) != 'E')
+			return false;
+		
+		char player = getCurrentPlayer(board);
+		board.setCell(row, col, player);
+		
+		return true;
+	}
+	
+		
+	
 	}
 
 
